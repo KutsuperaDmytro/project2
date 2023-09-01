24b7e31cf168a6ed4b896d418be4afb44b3e9ad2
Dmytro Kutsupera <139460609+KutsuperaDmytro@users.noreply.github.com>
2023-09-01 10:26:47 +0300

    Update clean_folder.py

--- Folder/clean_folder.py
+++ Folder/clean_folder.py
@@ -28,30 +28,30 @@ def handle_folder(folder: Path):
     except OSError:
         print(f"Can't delete folder: {folder}")
 
-def main(folder: Path):
-    parser.scan(folder)
+def main():
+    if len(sys.argv) > 1:
+        folder_for_scan = Path(sys.argv[1])
+        print(f'Start in folder: {folder_for_scan.resolve()}')
+        parser.scan(folder_for_scan)
+
     for file in parser.JPEG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPEG')
+        handle_media(file, parser.folder / 'images' / 'JPEG')
     for file in parser.JPG_IMAGES:
-        handle_media(file, folder / 'images' / 'JPG')
+        handle_media(file, parser.folder / 'images' / 'JPG')
     for file in parser.PNG_IMAGES:
-        handle_media(file, folder / 'images' / 'PNG')
+        handle_media(file, parser.folder / 'images' / 'PNG')
     for file in parser.SVG_IMAGES:
-        handle_media(file, folder / 'images' / 'SVG')
+        handle_media(file, parser.folder / 'images' / 'SVG')
     for file in parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3')
+        handle_media(file, parser.folder / 'audio' / 'MP3')
 
     for file in parser.MY_OTHER:
-        handle_media(file, folder / 'MY_OTHER')
+        handle_media(file, parser.folder / 'MY_OTHER')
     for file in parser.AECHIVES:
-        handle_media(file, folder / 'AECHIVES')
-
+        handle_media(file, parser.folder / 'AECHIVES')
 
     for folder in parser.FOLDERS[::-1]:
         handle_folder(folder)
 
 if __name__ == "__main__":
-     if sys.argv[1]:
-         folder_for_scan = Path(sys.argv[1])
-         print(f'Start in folder: {folder_for_scan.resolve()}')
-         main(folder_for_scan.resolve())
+    main()
